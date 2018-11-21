c1e51f3386d03fd6253c171b215a98a58e8730f1
RinuT <40629768+RinuT@users.noreply.github.com>
2018-11-21 13:23:38 +0530

    Update flextronics-notifications.js

--- flextronics-notifications/flextronics-notifications.js
+++ flextronics-notifications/flextronics-notifications.js
@@ -20,15 +20,98 @@ var app=angular.module('myApp.flextronics-notifications', ['ngRoute'])
 
 app.controller('flextronics-notifications-successCtrl', ['$scope','myservice','$http','$timeout',function ($scope,myservice,$http,$timeout) {
 
-  $scope.materialCode=" "
-  $scope.batchId=" "
-  $scope.poNumber=" "
-  $scope.invoiceId=" "
-  $scope.quantity=" "
-  $scope.perUnitPrice=" "
-  $scope.ppoStatus=" "
-  $scope.loading=true
-  $scope.notGenerated=[];
+  $scope.PODataUnpaid=[];
+  $scope.poData={}
+  $scope.poData.poNumber=""
+  $scope.poData.diffDays=""
+  $scope.poData.poStatus=""
+  $scope.poData.batchId=""
+  $scope.poData.materialCode=""
+  $scope.val="1"
+  $scope.search=false
+  $scope.BatchId=""
+  $scope.PONumber=""
+  $scope.LineNo=" "
+  $scope.price=" "
+  $scope.SEmaterialCode=" "
+  $scope.POQty=" "
+  $scope.UOP=" "
+  $scope.DeliveryDate=" "
+ $scope.Currency=" "
+ $scope.deliveryNoteNo=" "
+ $scope.shippmentDate=" "
+ $scope.shippedQty=" "
+ $scope.invoiceId=""
+ $scope.search=false
+$scope.Pending=false
+$scope.loading=true
+ var acc = document.getElementsByClassName("accordion");
+var i;
+$scope.CreationDate = new Date();
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+  
+  $scope.onload=function(){
+    
+    $http.get("http://ec2-35-173-231-185.compute-1.amazonaws.com:3000/api/newBatch")
+    .then(function(response) {
+      $scope.res=response.data
+      var datetime = new Date();
+      var date1 = new Date(datetime);
+      for( var n=0;n<$scope.res.length;n++){
+       
+        var date2 = new Date($scope.res[n].batch.receiptDate)
+        var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+        var diffDays = Math.ceil(timeDiff / (1000 * 3600* 24)); 
+        if(diffDays>90 & $scope.res[n].batch.availableQuantity>0){
+          $scope.poData['batch']=$scope.res[n].batchCode
+          $scope.poData['poNumber']=$scope.res[n].batch.poNumber
+          $scope.poData['materialCode']=$scope.res[n].batch.materialCode
+          $scope.poData['quantity']=$scope.res[n].batch.availableQuantity
+          $scope.poData['amount']=$scope.res[n].batch.amount
+          $scope.PODataUnpaid.push($scope.poData)
+          $scope.poData={}
+          $scope.Pending=true
+        
+        }
+
+       
+      }
+       if($scope.PODataUnpaid.length==0){
+          $scope.noData=true
+          
+        }
+        $scope.loading=false
+        
+    }, function(response) {
+      $scope.content = "Something went wrong";
+      $scope.loading=false
+  });
+  }
+ $scope.generateInvoice = function(id,batch,mc){
+  $scope.search=true
+  myservice.po=id
+  myservice.batch=batch
+   myservice.mc=mc
+ }
+ 
+ $scope.navigate=function(url){
+  window.location = url;
+
+}
+ }]);
+     app.controller('flextronics-notifications-successCtrl_new', ['$scope','myservice','$http','$timeout',function ($scope,myservice,$http,$timeout) {
+      $scope.notGenerated=[];
     $scope.inv="";
     $scope.PODataGenerated=[];
     $scope.PODataPaid=[];
@@ -39,6 +122,7 @@ app.controller('flextronics-notifications-successCtrl', ['$scope','myservice','$
     $scope.poData.diffDays=""
     $scope.poData.materialCode=""
     $scope.val="1"
+  $scope.noData=false
     $scope.Search=false
     $scope.Search1=false
     $scope.fialuier2=false
@@ -62,161 +146,9 @@ app.controller('flextronics-notifications-successCtrl', ['$scope','myservice','$
    $scope.display=true
    $scope.Pending=false
     $scope.PODataPending=[]
-    
-  $scope.fall=false
-    $scope.id=""
-    function init(){
-    var websocket =new WebSocket("ws://ec2-35-173-231-185.compute-1.amazonaws.com:3000");
-    websocket.addEventListener('open',evt =>doSocketOpen(evt));
-    websocket.addEventListener('message',evt =>doSocketMessage(evt));
-    websocket.addEventListener('close',evt =>doSocketClose(evt));
-    }
-    function doSocketClose(evt) {
-    console.log('Close.');
-    }
-    function doSocketMessage(evt) {
-      
-   let data =JSON.parse(evt.data);
-   $scope.materialCode=data.materialCode
-   $scope.batchId=data.batchCode
-   $scope.poNumber=data.poNumber
-   $scope.invoiceId=data.invDocNum
-   $scope.quantity=data.consumptionQuantity
-   $scope.perUnitPrice=data.invoiceAmount
-   $scope.ppoStatus=data.invoiceStatus
-    //window.location.reload();
-    $scope.loading=false
-    }
-    $scope.inv=myservice.xxx
-  
-    function doSocketOpen(evt) {
-    console.log('Open.');
-    }
-   $scope.onload=function(){
-    
-     init()
-      var requestInfo = Request();
-      var request=
-      {
-        "$class": "com.cts.ipm.p2pNetwork.searchInvoice",
-        "invDocNum": myservice.xxx
-  }  
-    var res = $http.post('http://ec2-35-173-231-185.compute-1.amazonaws.com:3000/api/searchInvoice',request).then(function successCallback(response){
-                 $scope.update_response=response;
-                 $scope.transactionId=$scope.update_response.data.transactionId
-                
-                 
-             }, function errorCallback(response){
-                 console.log("POST-ing of data failed");
-                 $scope.loading=false
-             });
-    
-       
-       function Request() {
-       
-         return {
-           "Request" : {
-            "$class": "com.cts.ipm.p2pNetwork.searchInvoice",
-            "invDocNum":""
-      }     
-           
-         };  
-         
-    }
-   }
-      
-    $scope.generate = function(){
-   
-     $scope.setValueInvoice();
-     }
-     $scope.setValueInvoice=function() {
-       var requestInvoice=
-       {
-        "$class": "com.cts.ipm.p2pNetwork.InvoiceStatus",
-        "invDocNum":  $scope.inv,
-        "invoiceStatus": "Paid",
-  "report": {
-    "$class": "com.cts.ipm.p2pNetwork.newReport",
-    "transactionType": "",
-    "date": "",
-    "quantity": "",
-    "poNumber": "",
-    "materialCode": ""
-  }
-        
-      }
-      
-        
-  
-         var requestInfo = RequestInvoice();
-       
-         data : requestInfo
-     
-       var res = $http.post('http://ec2-35-173-231-185.compute-1.amazonaws.com:3000/api/InvoiceStatus',requestInvoice).then(function successCallback(response){
-            //   alert("Successfully placed order");
-               $scope.update_response=response;
-              $scope.loading=true
-               $scope.Search1=true
-               $scope.sucess2=true
-               $scope.transactionId=$scope.update_response.data.transactionId
-               
-           }, function errorCallback(response){
-             $scope.fialuier3=true
-           });
-     }
-     $scope.navigate=function(url){
-      window.location = url;
-  
-    }
-     function RequestInvoice() {
-     
-       return {
-         "Request" : {
-          "$class": "com.cts.ipm.p2pNetwork.InvoiceStatus",
-          "invDocNum": " ",
-          "invoiceStatus": "Paid",
-          "report": {
-            "$class": "com.cts.ipm.p2pNetwork.newReport",
-            "transactionType": "",
-            "date": "",
-            "quantity": "",
-            "poNumber": "",
-            "materialCode": ""
-          }
-        }
-        
-         }
-       };
-  
-   
-     }]);
-     app.controller('flextronics-notifications-successCtrl_new', ['$scope','myservice','$http','$timeout',function ($scope,myservice,$http,$timeout) {
-      $scope.PODataUnpaid=[];
-      $scope.poData={}
-      $scope.poData.poNumber=""
-      $scope.poData.diffDays=""
-      $scope.poData.poStatus=""
-      $scope.poData.batchId=""
-      $scope.poData.materialCode=""
-      $scope.val="1"
-      $scope.search=false
-      $scope.BatchId=""
-      $scope.PONumber=""
-      $scope.LineNo=" "
-      $scope.price=" "
-      $scope.SEmaterialCode=" "
-      $scope.POQty=" "
-      $scope.UOP=" "
-      $scope.DeliveryDate=" "
-     $scope.Currency=" "
-     $scope.deliveryNoteNo=" "
-     $scope.shippmentDate=" "
-     $scope.shippedQty=" "
-     $scope.invoiceId=""
-     $scope.search=false
-    $scope.Pending=false
-       $scope.loading=true
-     var acc = document.getElementsByClassName("accordion");
+    $scope.loading=true
+    $scope.id= myservice.xxx
+    var acc = document.getElementsByClassName("accordion");
     var i;
     $scope.CreationDate = new Date();
     for (i = 0; i < acc.length; i++) {
@@ -230,115 +162,110 @@ app.controller('flextronics-notifications-successCtrl', ['$scope','myservice','$
         } 
       });
     }
-      function init(){
-      var websocket =new WebSocket("ws://ec2-35-173-231-185.compute-1.amazonaws.com:3000");
-      websocket.addEventListener('open',evt =>doSocketOpen(evt));
-      websocket.addEventListener('message',evt =>doSocketMessage(evt));
-      websocket.addEventListener('close',evt =>doSocketClose(evt));
-      }
-      function doSocketClose(evt) {
-      console.log('Close.');
-      }
-      function doSocketMessage(evt) {
-        let data={}
-      data =JSON.parse(evt.data);
     
-      
-        $scope.poData.batch=data.batch.batchCode
-        $scope.poData.poNumber=data.batch.batch.poNumber
-        $scope.poData.materialCode=data.batch.batch.materialCode
-        $scope.poData.quantity=data.batch.batch.availableQuantity
-        $scope.PODataUnpaid.push($scope.poData)
-        $scope.poData={}
-        $scope.Pending=true
-      
-      if($scope.PODataUnpaid.length==0){
-    $scope.fall=true
+   // $scope.inv=myservice.xxx
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+    $scope.CreationDate = new Date();
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+      });
     }
-        $scope.loading=false
-      }
-    
-      function doSocketOpen(evt) {
-      console.log('Open.');
-      }
-      init()
+   
       var requestInfo = Request();
       var request=
-                     {
-                      "$class": "com.cts.ipm.p2pNetwork.display"
-                }     
-      var res = $http.post('http://ec2-35-173-231-185.compute-1.amazonaws.com:3000/api/display',request).then(function successCallback(response){
+      {
+        "$class": "com.cts.ipm.p2pNetwork.generateInvoice",
+        "invDocNum": "1"+myservice.batch+"0",
+        "selfinvoice": {
+          "$class": "com.cts.ipm.p2pNetwork.SelfInvoice",
+          "consumptionQuantity": "",
+          "invoiceStatus": "",
+          "amount": "",
+          "materialCode": myservice.mc,
+          "poNumber": myservice.po,
+          "batch": {
+            "$class": "com.cts.ipm.p2pNetwork.newBatch",
+            "batchCode": " ",
+            "batch": {
+              "$class": "com.cts.ipm.p2pNetwork.batch",
+              "shipmentDate": "",
+              "receiptDate": "",
+              "shippedQuantity": "",
+              "recievedQuantity": "",
+              "availableQuantity": "",
+              "batchStatus": "",
+              "invoice": []
+            }
+          },
+          "invDocNum": " "
+        },
+        "batchCode": myservice.batch
+      }
+    var res = $http.post('http://ec2-35-173-231-185.compute-1.amazonaws.com:3000/api/generateInvoice',request).then(function successCallback(response){
                  $scope.update_response=response;
-                 $scope.sucess=true
                  $scope.transactionId=$scope.update_response.data.transactionId
                 
                  
              }, function errorCallback(response){
                  console.log("POST-ing of data failed");
-        $scope.loading=false
+                 $scope.loading=false
              });
     
        
        function Request() {
        
          return {
-           "Request" : {
-            "$class":  "com.cts.ipm.p2pNetwork.display"
-            
-           }
-           }
-         };
-         $scope.generate= function(){
+           "Request" :    {
+            "$class": "com.cts.ipm.p2pNetwork.generateInvoice",
+            "invDocNum": "",
+            "selfinvoice": {
+              "$class": "com.cts.ipm.p2pNetwork.SelfInvoice",
+              "consumptionQuantity": "",
+              "invoiceStatus": "",
+              "amount": "",
+              "materialCode": "",
+              "poNumber": "",
+              "batch": {
+                "$class": "com.cts.ipm.p2pNetwork.newBatch",
+                "batchCode": " ",
+                "batch": {
+                  "$class": "com.cts.ipm.p2pNetwork.batch",
+                  "shipmentDate": "",
+                  "receiptDate": "",
+                  "shippedQuantity": "",
+                  "recievedQuantity": "",
+                  "availableQuantity": "",
+                  "batchStatus": "",
+                  "invoice": []
+                }
+              },
+              "invDocNum": " "
+            },
+            "batchCode": ""
+          }
+           
+         };  
          
-     $scope.setValueInvoice();
-     }
-     $scope.generateInvoice = function(id){
-      $scope.search=true
-      myservice.xxx=id
-     }
-     
+    }
+
      $scope.navigate=function(url){
       window.location = url;
-    
+  
     }
-     function RequestInvoice() {
-     
-       return {
-         "Request" : {
-          "$class": "com.cts.ipm.p2pNetwork.generateInvoice",
-          "invDocNum": "",
-          "selfinvoice": {
-            "$class": "com.cts.ipm.p2pNetwork.SelfInvoice",
-            "consumptionQuantity": "",
-            "invoiceStatus": "",
-            "amount": "",
-            "materialCode": "",
-            "poNumber": "",
-            "batch": {
-              "$class": "com.cts.ipm.p2pNetwork.newBatch",
-              "batchCode": " ",
-              "batch": {
-                "$class": "com.cts.ipm.p2pNetwork.batch",
-                "shipmentDate": "",
-                "receiptDate": "",
-                "shippedQuantity": "",
-                "recievedQuantity": "",
-                "availableQuantity": "",
-                "batchStatus": "",
-                "invoice": []
-              }
-            },
-            "invDocNum": " "
-          },
-          "batchCode": ""
-        }
     
-         }
-       };
-       
-         }]);
-      
+     }]);
+  
         
     app.service('myservice', function() {
-      this.xxx = "yyy";
+      this.mc = "";
+      this.po = "";
+      this.batch=""
     });
